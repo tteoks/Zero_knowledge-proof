592dfa73d4773890636693f1a8f84f0afdd93497
tteoks <tteoks01@naver.com>
2021-01-07 06:24:57 -0800

    Update 2021.01.07

---
+++ crypto/crypto.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+#include<openssl/sha.h>
+
+bool simpleSHA256(void *input, unsigned long length, unsigned char *hash);
+void trans_string(unsigned char hash[SHA256_DIGEST_LENGTH], char *output);
