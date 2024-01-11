a270280431d3712aa231172163dd0780fff75fd5
jinho Yu <yujinho000@gmail.com>
2024-01-11 10:45:29 -0500

    Update data generator script
    * several functions turn into modules in common.py

--- database/gen_clientdataset.py
+++ database/gen_clientdataset.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+from pymysql import connect
 from common import *
 
-# Edit these values to your server env
-DB_HOST = "126.0.0.1"
+# Edit these values to your client env
+DB_HOST = "127.0.0.1"
 DB_USER = "admin"
 DB_PASS = ""
-DB_NAME = "KCDC"
-DB_TABLE = "_PrivateSetY"
-	
+DB_NAME = "airline"
+DB_TABLE = "_PrivateSetX"
+
 if __name__=="__main__":
-    dataset = make_dataset(3000, preset_values)
+    dataset = make_dataset(2000, preset_values)
 
     conn = connect(host = DB_HOST, user = DB_USER, password = DB_PASS, db = DB_NAME, charset = 'utf8')
     INSERT_DB(dataset, conn, DB_TABLE)
@@ -19,4 +20,3 @@ if __name__=="__main__":
 
     for i in res:
         print(i)
-
